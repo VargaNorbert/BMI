d730ebcb54d5b4c6a171c37928a4baecce1ee992
VargaNorbert <VargaNorbert@users.noreply.github.com>
2022-09-20 11:08:28 +0000

    deploy: 0bfd5d1a3f135e34772df2ab667f50a8368e2e8c

---
+++ main.js
@@ -0,0 +1,52 @@
+function katt(){
+
+    let szam=parseFloat( document.getElementById('suly').value / (document.getElementById('magassag').value/100)**2).toFixed(2);
+
+    document.getElementById('index').innerHTML = szam;
+
+    if(szam<parseFloat(18,5)){
+        document.getElementById('elso').style.backgroundColor= '#f3953e';
+        document.getElementById('masodik').style.backgroundColor= 'white';
+        document.getElementById('harmadik').style.backgroundColor= 'white';
+        document.getElementById('negyedik').style.backgroundColor= 'white';
+        document.getElementById('otodik').style.backgroundColor= 'white';
+        document.getElementById('hatodik').style.backgroundColor= 'white';
+    }else if(szam<25){
+        document.getElementById('masodik').style.backgroundColor= '#f3953e';
+        document.getElementById('elso').style.backgroundColor= 'white';
+        document.getElementById('harmadik').style.backgroundColor= 'white';
+        document.getElementById('negyedik').style.backgroundColor= 'white';
+        document.getElementById('otodik').style.backgroundColor= 'white';
+        document.getElementById('hatodik').style.backgroundColor= 'white';
+    }else if(szam<30){
+        document.getElementById('harmadik').style.backgroundColor= '#f3953e';
+        document.getElementById('masodik').style.backgroundColor= 'white';
+        document.getElementById('elso').style.backgroundColor= 'white';
+        document.getElementById('negyedik').style.backgroundColor= 'white';
+        document.getElementById('otodik').style.backgroundColor= 'white';
+        document.getElementById('hatodik').style.backgroundColor= 'white';
+    }else if(szam<35){
+        document.getElementById('negyedik').style.backgroundColor= '#f3953e';
+        document.getElementById('masodik').style.backgroundColor= 'white';
+        document.getElementById('harmadik').style.backgroundColor= 'white';
+        document.getElementById('elso').style.backgroundColor= 'white';
+        document.getElementById('otodik').style.backgroundColor= 'white';
+        document.getElementById('hatodik').style.backgroundColor= 'white';
+    }else if(szam<40){
+        document.getElementById('otodik').style.backgroundColor= '#f3953e';
+        document.getElementById('masodik').style.backgroundColor= 'white';
+        document.getElementById('harmadik').style.backgroundColor= 'white';
+        document.getElementById('negyedik').style.backgroundColor= 'white';
+        document.getElementById('elso').style.backgroundColor= 'white';
+        document.getElementById('hatodik').style.backgroundColor= 'white';
+    }else{
+        document.getElementById('hatodik').style.backgroundColor= '#f3953e';
+        document.getElementById('masodik').style.backgroundColor= 'white';
+        document.getElementById('harmadik').style.backgroundColor= 'white';
+        document.getElementById('negyedik').style.backgroundColor= 'white';
+        document.getElementById('otodik').style.backgroundColor= 'white';
+        document.getElementById('elso').style.backgroundColor= 'white';
+    }
+}
+
+document.getElementById('gomb').addEventListener('click',katt);
